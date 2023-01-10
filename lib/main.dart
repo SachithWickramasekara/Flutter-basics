08de35c682f84984196334ad6417afc0dae71062
SachithWickramasekara <sachithwickramasekara@gmail.com>
2023-01-11 00:18:40 +0530

    start learning flutter

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(
+    const Center(
+      child: Text(
+        "This is my first flutter project",
+        textDirection: TextDirection.rtl,
+        )
+      ),
+  );
+}
